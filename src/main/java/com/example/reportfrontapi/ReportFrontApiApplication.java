0a5b7e70c94e41255af1dc8c836b54906a5354a2
seculoper <mir8725@naver.com>
2026-06-19 02:16:43 +0900

    project init.

---
+++ src/main/java/com/example/reportfrontapi/ReportFrontApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.reportfrontapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReportFrontApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReportFrontApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/reportfrontapi/ReportFrontApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reportfrontapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReportFrontApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
